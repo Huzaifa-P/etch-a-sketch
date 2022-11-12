3492067fa1474a1f9d09276e159e5141ab03ba83
Huzaifa Patel <huzaifa.patel99@hotmail.com>
2022-11-12 17:26:57 +0000

    finished adding controls

--- script.js
+++ script.js
@@ -1,48 +1,131 @@
 const container = document.querySelector('.gridContainer');
-const btnSize = document.querySelector('.size');
-
-const slider = document.querySelector('.slider');
-const sliderVal = document.querySelector('.sliderVal');
-sliderVal.textContent = slider.value + ' x ' + slider.value;
-
-slider.oninput = () => {
-    sliderVal.textContent = slider.value + ' x ' + slider.value;
-}
-
 let mouseClick = false;
 
+// pen states
+let isDefault = true;
+let isPastel = false;
+let isBlack = false;
+let isEraser = false;
+
+// pen buttons
+const btnDefault = document.querySelector('.default')
+const btnBlack = document.querySelector('.black');
+const btnPastel = document.querySelector('.pastel');
+const btnErase = document.querySelector('.erase');
+
+// change pen state to default
+btnDefault.addEventListener('click', () => {
+    isBlack = false;
+    isDefault = true;
+    isPastel = false;
+    isEraser = false;
+});
+
+// change pen state to black
+btnBlack.addEventListener('click', () => {
+    isBlack = true;
+    isDefault = false;
+    isPastel = false;
+    isEraser = false;
+});
+
+// change pen state to rainbow
+btnPastel.addEventListener('click', () => {
+   isPastel = true;
+   isDefault = false;
+   isBlack = false;
+   isEraser = false;
+});
+
+btnErase.addEventListener('click', () => {
+    isPastel = false;
+    isDefault = false;
+    isBlack = false;
+    isEraser = true;
+});
+// change color to random rgb colors
+let setPastel = () => {
+    const pastels = {
+        1: '#89AEB2',
+        2: '#97F2F3',
+        3: '#F1E0B0',
+        4: '#F1CDB0',
+        5: '#E7CFC8'
+    }
+
+    return pastels[Math.floor(Math.random() * (5 - 1 + 1)) + 1]
+}
+
+// create grid on website load
 let createGrid = (size) => {
     container.style.gridTemplateColumns = `repeat(${size}, auto)`;
     for (let i = 0; i < size * size; i++) {
         const gridDiv = document.createElement('div');
         gridDiv.classList.add(`gridBox`);
         gridDiv.draggable = false;
-        gridDiv.addEventListener('mousedown', () => {
-            if (mouseClick === false) {
-                mouseClick = true;
-                gridDiv.style.backgroundColor = 'blue';
-            }
-            else {
-                mouseClick = false;
-            }
-        });
-        gridDiv.addEventListener('mouseover', () => {
-            if (mouseClick === true) gridDiv.style.backgroundColor = 'blue';
-        });
+        gridDiv.style.border = '1px solid rgba(0, 0, 0, 0.1)';
+        gridDiv.addEventListener('mousedown', changeColor);
+        gridDiv.addEventListener('mouseover', changeColor);
         container.append(gridDiv);
     }
 }
 
+// set color and paint grid box
+let changeColor = (e) => {
+    let pen = ''
+    if (isDefault) pen = 'blue';
+    if (isPastel) pen = setPastel();
+    if (isBlack) pen = 'black';
+    if (isEraser) pen = 'white';
+
+    if (e.type === 'mousedown') {
+        if (mouseClick === false) {
+            mouseClick = true;
+            e.path[0].style.backgroundColor = pen;
+        }
+        else {
+            mouseClick = false;
+        }
+    }
+    else if (e.type === 'mouseover') {
+        if (mouseClick === true) e.path[0].style.backgroundColor = pen;
+    }
+}
+createGrid(32);
+
+const btnSize = document.querySelector('.size'); // get size button
+const btnClear = document.querySelector('.clearGrid'); // get clear button
+
+const slider = document.querySelector('.slider'); // get slider
+const sliderVal = document.querySelector('.sliderVal'); // get slider text
+sliderVal.textContent = slider.value + ' x ' + slider.value; // initialise slider text
+
+slider.oninput = () => {
+    sliderVal.textContent = slider.value + ' x ' + slider.value; // set slider value text
+}
+
+let size = 32;
+
+// used in changeSize and clearGrid
 let removeGrid = () => {
     let boxes = container.querySelectorAll('div');
     boxes.forEach(box => container.removeChild(box));
 }
 
+// alter size value then create new grid
 let changeSize = () => {
-    let size = slider.value;
+    if (size !== slider.value) {
+        size = slider.value;
+        removeGrid();
+        createGrid(size);
+    }
+}
+
+// clears colors from grid
+let clearGrid = () => {
     removeGrid();
     createGrid(size);
 }
 
-createGrid(32);
+btnClear.addEventListener('click', clearGrid);
 btnSize.addEventListener('click', changeSize);
